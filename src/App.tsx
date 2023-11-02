cf7df1e64d07f439c924d18cf76814e22b85b96b
deividsonsabino <deividsonsabino@outlook.com.br>
2023-11-02 20:29:55 -0300

    fix :bug:  : fix inpunts and states

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,12 @@ import { useState } from 'react'
 import './App.scss'
 
 function App() {
-  const [height, setHeight] = useState("");
-  const [width, setWidth] = useState("");
-  const [imc, setImc] = useState(0);
+  const [height, setHeight] = useState<number>(0);
+  const [width, setWidth] = useState<number>(0);
   const [imcStatus, setImcStatus] = useState("");
 
   function calcImc() {
     const imcValue = Number((width / (height * height)).toFixed(2))
-    console.log(imc)
     if (imcValue > 0) {
       if (imcValue <= 18.5) {
         setImcStatus("Magreza")
@@ -26,30 +24,36 @@ function App() {
       } else {
         setImcStatus("Valor informado incorreto")
       }
-      setImc(imcValue);
-      console.log(imcStatus)
     }
   }
 
   return (
     <>
-      <h1 >Calculadora de IMC</h1>
+      <h1>Calculadora de IMC</h1>
       <div className="card">
         <span>Informe seu peso (kg)</span>
         <input
-          value={width}
-          onChange={(e) => setWidth(e.target.value)}
+          value={width || ''}
+          onChange={(e) => setWidth(Number(e.target.value))}
+          type="number"
         />
         <span>Informe sua altura (metro e cm)</span>
         <input
-          value={height}
-          onChange={(e) => setHeight(e.target.value)}
+          value={parseFloat(height.toFixed(2)) || ''}
+          onChange={(e) => setHeight(Number(e.target.value))}
+          type="number"
         />
         <p>{imcStatus}</p>
-        <button onClick={calcImc}>CALCULAR</button>
+        {width > 0 && height > 0 &&
+          <button onClick={calcImc}>CALCULAR</button>
+        }
       </div>
     </>
   )
 }
 
 export default App
+
+
+
+
